df3b5c0b90a793844db1c033e994a94d3999e3e7
MOSTAFA837 <mostafamanchester837@gmail.com>
2022-05-10 19:39:31 +0200

    Orders

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -90,10 +90,11 @@ const SingleProduct = ({ product, onStarClick, star }) => {
 
         <Card
           actions={[
-            <Tooltip title={tooltip}>
-              <a onClick={handleAddToCart}>
-                <ShoppingCartOutlined className="text-danger" /> <br /> Add to
-                Cart
+            <Tooltip placement="top" title={tooltip}>
+              <a onClick={handleAddToCart} disabled={product.quantity < 1}>
+                <ShoppingCartOutlined className="text-danger" />
+                <br />
+                {product.quantity < 1 ? "Out of Stock" : "Add To Cart"}
               </a>
             </Tooltip>,
             <Link to="/">
